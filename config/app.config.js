999efaf6cd1ad0f8b3add23355591498ada802ef
Agafina <98034773+Agafina@users.noreply.github.com>
2023-06-24 12:06:32 -0800

    hello

---
+++ config/app.config.js
@@ -0,0 +1,23 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://fowedlungatsafac:agafina@cluster0.x3nwz3m.mongodb.net/Cluster0?retryWrites=true&w=majority'
+}
+
+module.exports = {
+   MONGO_DB_CONFIG
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
